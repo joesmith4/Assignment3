dbd1108e39f20a69ef75b00e8d451d4697922990
Joseph Smith <jdsmith4@mix.wvu.edu>
2018-09-16 14:33:10 -0400

    Made minor changes to variable names

--- CanDecoder.cpp
+++ CanDecoder.cpp
@@ -11,14 +11,14 @@ return static_cast<int>(longMessage);
 }
 
 int main() {
-unsigned char abc[4][5] ={{0x01, 0x40, 0x00, 0x00, 0x00},
+unsigned char canMessages[4][5] ={{0x01, 0x40, 0x00, 0x00, 0x00},
                           {0x06, 0x40, 0x00, 0x00, 0x00},
                           {0x09, 0x60, 0x00, 0x00, 0x00},
                           {0x0C, 0x80, 0x00, 0x00, 0x00}};
-printf("Message 1: %d \n", DecodeCan(abc[0]));
-printf("Message 2: %d \n", DecodeCan(abc[1]));
-printf("Message 3: %d \n", DecodeCan(abc[2]));
-printf("Message 4: %d \n", DecodeCan(abc[3]));
+printf("Message 1: %d \n", DecodeCan(canMessages[0]));
+printf("Message 2: %d \n", DecodeCan(canMessages[1]));
+printf("Message 3: %d \n", DecodeCan(canMessages[2]));
+printf("Message 4: %d \n", DecodeCan(canMessages[3]));
 return(0);
 }
 
